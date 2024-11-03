3e6050df420fc92e86ce54f7fba98a0331ed1266
Mio Nekoya <admin@mio9.sh>
2024-11-03 23:20:06 +0900

    add async function

--- index.ts
+++ index.ts
@@ -9,3 +9,16 @@ export const five = 5;
 export function giveMeFive() {
     return five;
 }
+
+/**
+ * Gives you a five after a delay, default to 5 seconds
+ * @param delay
+ * @returns 5
+ */
+export async function giveMeFiveLater(delay: number): Promise<number> {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(5)
+        }, delay || 5000);
+    })
+}
